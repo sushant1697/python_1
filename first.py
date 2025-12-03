38a36bf9215ef4252344bfa8a756c6d0991c89d5
Sushant Chadha <sushantchadhain@gmail.com>
2025-12-03 21:07:54 +0530

    5th commit

--- first.py
+++ first.py
@@ -2,4 +2,9 @@ print("This is the first.py file.")
 
 def chai(n):
     print(n)
-chai("Test 1122")
+
+chai("Hello this is try code")
+
+chai_one = "Giner tea"
+chai_two = "Rose tea"
+chai_three = "Lemon tea"
